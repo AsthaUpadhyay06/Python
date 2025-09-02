6bca62bdbfb6ad19f373cbc1e6944dab3a3b2b51
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-09-02 23:45:08 +0530

    Calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operation (+, -, *, /): ")
+
+if op == '+':
+    print(a + b)
+elif op == '-':
+    print(a - b)
+elif op == '*':
+    print(a * b)
+elif op == '/':
+    print(a / b if b != 0 else "Cannot divide by zero")
+else:
+    print("Invalid operation")
