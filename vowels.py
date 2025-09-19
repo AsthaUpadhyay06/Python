16f93397f4bbf38860ad31b9981e9b829f63c2f8
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-09-19 23:00:43 +0530

    GCD

--- vowels.py
+++ vowels.py
@@ -1,9 +1,14 @@
 text = input("Enter a string: ")
 vowels = "aeiouAEIOU"
-count = 0
+v_count = 0
+c_count=0
 
 for char in text:
-    if char in vowels:
-        count += 1
+    if char.isalpha(): 
+      if char in vowels:
+        v_count+=1
+    else:
+        c_count+=1
 
-print("Number of vowels:", count)
+print("Number of vowels:", v_count)
+print("Number of consonants:", c_count)
