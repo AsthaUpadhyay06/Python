530f910db541ad05ff42ac808c4c437c07f14780
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-10-10 15:10:12 +0530

    python codes

---
+++ genai2.py
@@ -0,0 +1,95 @@
+# a = 45
+# b = 56
+# c = 67
+
+# if (a > b and a > c):
+#     print("A is largest")
+# elif (b > a and b > c):
+#     print("B is largest")
+# else:
+#     print("C is largest")
+
+
+# 
+
+# num=5613
+# sum=0
+# count=0
+# while(num>0):
+#      dig=num%10
+#      sum=sum+dig
+#      num=num//10
+#      count=count+1
+     
+# print("sum is",sum) 
+# print("count is",count)   
+# # 
+
+
+# num=66
+# flag=0
+# for i in range(12,num):
+#     if(num%i==0):
+#         flag=1
+        
+#     if(flag==0):
+#         print("prime")
+#         break
+#     else:
+#         print("not prime")
+#         break
+    
+
+    # factorial
+
+
+# fact=1
+# n=5
+# for i in range(1,n+1):
+#     fact=fact*i
+# print("factorial",fact)
+       
+    
+    #    print prime number jo 3 se 10 k beach me ho
+  
+
+for i in range(3,10):
+    flag=0
+    for j in range(2,i):
+        if(i%j==0):
+            flag=1
+            break
+        if(flag==0):
+            print(i)
+        else:
+            print("not prime")
+            break
+
+
+
+           
+        
+
+
+
+
+
+# for num in range(12,77):
+#     flag=0
+#     for i in range(2,num):
+#         if(num%i==0):
+#             flag=1
+#             break
+#     if(flag==0):
+#         print(num)
+#     else:
+#         print("not prime")
+#         break
+
+       
+            
+
+
+    
+       
+        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,19 @@
-print("Reversed words:", reversed_words)
+or num in range(12,77):
+#     flag=0
+#     for i in range(2,num):
+#         if(num%i==0):
+#             flag=1
+#             break
+#     if(flag==0):
+#         print(num)
+#     else:
+#         print("not prime")
+#         break
+
+       
+            
+
+
+    
+       
+        
