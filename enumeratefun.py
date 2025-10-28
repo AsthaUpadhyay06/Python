fec935f7256c66db981273ad760d389dddc52df3
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-10-28 15:20:34 +0530

    isinstancefunction()

---
+++ enumeratefun.py
@@ -0,0 +1,4 @@
+num=[10,20,30,40]
+//indexing khi se bhi start kr skte ho
+for index,value in enumerate(num,10):
+    print("")
---
+++ filterfunction.py
@@ -0,0 +1,5 @@
+def find_pos_num(num):
+    if num > 0:
+        return True
+    else:
+        return False
