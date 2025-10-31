17dee8e610925c38daa7a165dab2f0d8d09a6566
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-10-31 13:17:30 +0530

    lambdafunction

---
+++ lambdafunction.py
@@ -0,0 +1,8 @@
+
+# sumofdigits=lambda n:sum(int(digit)
+#  for digit in str(n))
+
+# print("Sum of digits:",sumofdigits(12345)) 
+
+# double=lambda x:x*4
+# print(double(4))
---
+++ recursioneven.py
@@ -0,0 +1,5 @@
+recursioin me even number print krna hai
+def recursion_even(n):
+    if n%2==0:
+     print(n)
+
