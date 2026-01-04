c5675969da3b42fcc3790f25e354715ce20cdc2f
Astha Upadhyay <asthaupadhyay046@gmail.com>
2026-01-04 23:55:35 +0530

    gg

---
+++ ggh.py
@@ -0,0 +1,11 @@
+inches=float(input("enter measurement in inches: "))  
+foot=inches/12
+yard=inches/36
+cm=inches*2.54
+meter=inches/39.37
+
+print("measurement in foot:",foot)
+print("measurement in yard:",yard)
+print("measurement in cm:",cm)
+print("measurement in meter:",meter)
+
