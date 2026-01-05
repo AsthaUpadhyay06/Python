03b0059a4beeb4774741397394dc064e69da7bc4
Astha Upadhyay <asthaupadhyay046@gmail.com>
2026-01-05 23:53:15 +0530

    tt

---
+++ sd.py
@@ -0,0 +1,12 @@
+ = int(input("Enter number of rows: "))
+
+for i in range(n):
+    print(" " * (n - i), end="")
+    
+    
+    val = 1
+    for j in range(i + 1):
+        print(val, end=" ")
+        val = val * (i - j) // (j + 1)  
+    
+    print() 
